c9825ce35d01614c9beb799ffa7fb503dfbae1e2
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2020-09-03 12:04:10 +0200

    fix(jest): fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     preset: 'ts-jest',
     clearMocks: true,
-    testMatch: '<rootDir>/src/**/*.spec.(ts|tsx)',
+    testMatch: [
+        '<rootDir>/src/**/*.spec.(ts|tsx)',
+    ],
     collectCoverage: true,
     coverageDirectory: '<rootDir>/coverage'
 }
