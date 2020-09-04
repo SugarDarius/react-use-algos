0d262a460f58a44b39dfa0b3ca30504ac541a21d
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2020-09-04 10:31:47 +0200

    config(storybook): update config

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,12 @@
+import { themes } from '@storybook/theming';
+
 export const parameters = {
     options: {
         storySort: {
             order: ['Greetings', 'DataTypes', 'Algos'],
         }
+    },
+    docs: {
+        theme: themes.dark,
     }
 };
