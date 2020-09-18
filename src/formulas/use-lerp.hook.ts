efd99825b8521e5939b9e0201d1c843489513570
Aurelien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2020-09-18 14:19:42 +0200

    refactor(hooks): add promise in use lerp for non blocking main thread

--- src/formulas/use-lerp.hook.ts
+++ src/formulas/use-lerp.hook.ts
@@ -10,7 +10,9 @@ export function useLerp(v0: number, v1: number, t: number): UseLerpReturnType {
 
 	const compute = React.useCallback(
 		(v0: number, v1: number, t: number): void => {
-			setValue((1 - t) * v0 + t * v1);
+			Promise.resolve().then((): void => {
+				setValue((1 - t) * v0 + t * v1);
+			});
 		},
 		[v0, v1, t]
 	);
