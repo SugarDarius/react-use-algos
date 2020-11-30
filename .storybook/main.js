96f1b9ec98297388aa009b551bce33104ff50469
Aurelien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2020-11-30 13:58:04 +0100

    feat(packages): upgrade packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,16 @@
 module.exports = {
-    stories: [
-        '../src/**/*.stories.mdx',
+	stories: [
+		'../src/**/*.stories.mdx',
         '../src/**/*.stories.@(ts|tsx)',
-    ],
-    addons: [
-        {
+	],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		{
             name: '@storybook/addon-docs',
             options: {
                 transcludeMarkdown: true
             }
         },
-    ]
+	],
 };
